1ba646123e433a0e9dc6d2d8678de46f9678a0fc
thaliawijaya03 <thaliawijaya03@gmail.com>
2025-08-08 00:56:14 +0800

    add initial HTML structure and JavaScript functionality for Todo List application

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+
+function validateForm() {
+    const todoInput = document.getElementById('todo-input'); value.trim();
+    const dateInput  = document.getElementById('date-input'); value;
+
+   console.log('Todo Input:', todoInput);
+    console.log('Date Input:', dateInput);
+}
